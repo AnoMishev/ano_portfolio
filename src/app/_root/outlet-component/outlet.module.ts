09d00849f1af9585b608eec956d259c6e0363f4d
AnoMishev <ano.mishev@gmail.com>
2024-09-19 01:31:58 +0200

    another-design

--- src/app/_root/outlet-component/outlet.module.ts
+++ src/app/_root/outlet-component/outlet.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from "@angular/core";
-import { HeaderComponent } from "./header/header.component";
-import { MainComponent } from "./main/main.component";
-import { FooterComponent } from "./footer/footer.component";
 import { CommonModule } from "@angular/common";
 import { OutletRouter } from "./outlet.router";
 import { OutletComponent } from "./outlet.component";
@@ -9,9 +6,6 @@ import { OutletComponent } from "./outlet.component";
 @NgModule({
     declarations: [
         OutletComponent,
-        HeaderComponent,
-        MainComponent,
-        FooterComponent
     ],
     imports: [
         CommonModule,
